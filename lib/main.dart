cc606153f5266c94a7c5828569e928b51bb1d54b
Siavash Mirzaei-Fard <63229654+siavashmirzaeifard@users.noreply.github.com>
2022-04-16 15:17:00 +0430

    lastCommit
    Removed unused comments in the application.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:flutter/material.dart';
+
 import 'package:flutter/services.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:sqflite/sqflite.dart';
 
